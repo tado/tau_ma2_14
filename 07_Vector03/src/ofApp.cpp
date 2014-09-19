4be70ff298124059dcfd47e9fd5d76e5cd789196
Atsushi Tadokoro <tadokoro@gmail.com>
2014-09-19 13:42:03 +0900

    update

--- 07_Vector03/src/ofApp.cpp
+++ 07_Vector03/src/ofApp.cpp
@@ -104,7 +104,7 @@ void ofApp::mousePressed(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-    
+    setInit();
 }
 
 //--------------------------------------------------------------
--- 07_Vector03/src/ofApp.h
+++ 07_Vector03/src/ofApp.h
@@ -19,15 +19,15 @@ public:
     void dragEvent(ofDragInfo dragInfo);
     void gotMessage(ofMessage msg);
     
-    void setInit(); // 初期設定
-    void resetForce(); // 力をリセット
+    void setInit();     // 初期設定
+    void resetForce();  // 力をリセット
     void updateForce(); // 力を更新
-    void updatePos(); // 位置の更新
+    void updatePos();   // 位置の更新
     void checkBounds(); // 画面からはみ出たらバウンドさせる
     
     static const int CIRCLE_NUM = 400; // 描画する円の数を指定
-    ofVec2f position[CIRCLE_NUM]; // 位置ベクトルの配列
-    ofVec2f velocity[CIRCLE_NUM]; // 速度ベクトルの配列
-    ofVec2f force[CIRCLE_NUM]; // 力ベクトルの配列
-    float friction = 0.01; // 摩擦係数
+    ofVec2f position[CIRCLE_NUM];      // 位置ベクトルの配列
+    ofVec2f velocity[CIRCLE_NUM];      // 速度ベクトルの配列
+    ofVec2f force[CIRCLE_NUM];         // 力ベクトルの配列
+    float friction = 0.01;             // 摩擦係数
 };
